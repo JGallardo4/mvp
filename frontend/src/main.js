d4e922cf775693e32c798a494a358bf6f3ef127d
Juan Gallardo <juan.gallardo.ab@gmail.com>
2021-03-24 21:46:17 -0600

    Replace client with new Nuxt app

--- frontend/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import router from "./router/index.js";
-import App from "./App.vue";
-import "./assets/css/reset.css";
-
-createApp(App)
-  .use(router)
-  .mount("#app");
